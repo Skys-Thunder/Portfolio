575a552984ed542989aaf7faaee8a1237ea7225b
Sky_Thunder <107789489+Skys-Thunder@users.noreply.github.com>
2023-09-07 22:40:47 +0900

    Create header.js

---
+++ header.js
@@ -0,0 +1,20 @@
+/*
+        <div id="header">
+            <h1 id="top-title">Sky_Thunder</h1>
+            <ul id="headers">
+                <li>About</li>
+                <li>Member</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+*/
+
+hse = document.querySelector("#main");
+hse.insertAdjacentHTML("afterbegin", `        <div id="header">
+            <h1 id="top-title">Sky_Thunder</h1>
+            <ul id="headers">
+                <li>About</li>
+                <li>Member</li>
+                <li>Contact</li>
+            </ul>
+        </div>`);
