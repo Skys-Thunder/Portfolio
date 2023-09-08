8f92d0ba17d33e5129c606fe498dcb19fed70254
Sky_Thunder <107789489+Skys-Thunder@users.noreply.github.com>
2023-09-08 22:21:15 +0900

    Update header.js

--- header.js
+++ header.js
@@ -13,8 +13,8 @@ hse = document.querySelector("#main");
 hse.insertAdjacentHTML("beforebegin", `        <div id="header">
             <h1 id="top-title">Sky_Thunder</h1>
             <ul id="headers">
-                <li>About</li>
-                <li>Member</li>
-                <li>Contact</li>
+                <li><a href="/article">Article</a></li>
+                <li><a href="/tools">Tools</a></li>
+                <li><a href="/concact">Contact</a></li>
             </ul>
         </div>`);
